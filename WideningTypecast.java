2e6e20a1b7db7bc6984115bd4c7c4df2e260f003
Gauri Bhagwat <157735967+gauri6011@users.noreply.github.com>
2024-02-13 22:19:01 +0530

    Add files via upload

---
+++ WideningTypecast.java
@@ -0,0 +1,14 @@
+class WideningTypecast
+{
+public static void main (String[] wtc)
+{
+//Implicit typecast 
+int a=10;
+float b=a;
+System.out.println(b);
+//explicit tyoecast
+int a1=10;
+float b1=(float)a1;
+System.out.println(b1);
+}
+}
