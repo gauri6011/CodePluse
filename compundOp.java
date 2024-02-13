90e939f14e2a291bd85014dfb9f530fd7b070bfc
Gauri Bhagwat <157735967+gauri6011@users.noreply.github.com>
2024-02-13 22:48:06 +0530

    Add files via upload

---
+++ compundOp.java
@@ -0,0 +1,15 @@
+class compundOp {
+    public static void main(String[] args) {
+        float age1=3.5f;
+        int age=15;
+// compound assignment Operator (-=)
+        age-=age1;
+        System.out.println(age);
+    }
+}
+
+/*output:
+compile time successful
+Runtime successful
+4
+ */
