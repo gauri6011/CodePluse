c3f4b5145beca84c445e43eeb25997c78310c677
Gauri Bhagwat <157735967+gauri6011@users.noreply.github.com>
2024-02-14 23:00:55 +0530

    Add files via upload

---
+++ DoubleToChar.java
@@ -0,0 +1,13 @@
+//Convert double to char
+public class DoubleToChar {
+    public static void main(String[] args) {
+        double a=123.4;
+        char b=(char)a;
+        System.out.println(b);
+    }
+}
+
+/*OutPut:
+ Compile Time Successful
+ Run Time Successful: {
+ */
