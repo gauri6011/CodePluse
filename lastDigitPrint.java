fd3f8c128cdf305eebacac36a485335cb7556122
Gauri Bhagwat <157735967+gauri6011@users.noreply.github.com>
2024-02-14 23:07:09 +0530

    Add files via upload

---
+++ lastDigitPrint.java
@@ -0,0 +1,14 @@
+// print last digit 
+public class lastDigitPrint {
+    public static void main (String[]ldp){
+        int a=12345;
+        int value=a%10;
+        System.out.println("The Last Digit is :" +value);
+    }
+}
+
+/*Output:
+  Compile time successful
+  Run time successful
+  The Last Digit is :5
+ */
