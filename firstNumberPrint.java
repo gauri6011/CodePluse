a07451af3b07a57fbd005fbb384b0f0222833eff
Gauri Bhagwat <157735967+gauri6011@users.noreply.github.com>
2024-02-15 23:37:12 +0530

    Add files via upload

---
+++ firstNumberPrint.java
@@ -0,0 +1,14 @@
+public class firstNumberPrint {
+    public static void main(String [] fnp){
+        int value1=8765595;
+        int value2= value1 / 10;
+        System.out.println(value2);
+    }
+    
+}
+/*output
+compile time success
+run time success
+876559
+ * 
+ */
