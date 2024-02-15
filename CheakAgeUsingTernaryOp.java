5115f40c099e8723cbb0c5ff67aa289322075229
Gauri Bhagwat <157735967+gauri6011@users.noreply.github.com>
2024-02-15 23:06:04 +0530

    Add files via upload

---
+++ CheakAgeUsingTernaryOp.java
@@ -0,0 +1,17 @@
+class CheakAgeUsingTernaryOp {
+    public static void main(String[] args) {
+        int k=21;
+        int d=88;
+        int r=90;
+        int s=20;
+        
+        int Result= (k>d) ? ((k>r)? ((k>s) ? (k):(s)) :(r>s ? (r) :(s))):(d>r ? ((d>s)? (d): (s)):(r>s)?(r):(s));
+        System.out.println("Result :" +Result);
+    } 
+}
+
+/*output:
+ Compile time success
+ Run time success
+ Result :90
+ */
