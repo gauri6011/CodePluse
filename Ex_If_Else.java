770514a2c56eab69de30b4dae66409840fdaae10
Gauri Bhagwat <157735967+gauri6011@users.noreply.github.com>
2024-02-27 13:32:57 +0530

    Add files via upload

---
+++ Ex_If_Else.java
@@ -0,0 +1,20 @@
+ class Ex_If_Else {
+    public static void main (String []args){
+        int money =200;
+        if(money>=150){
+            System.out.println("You can buy tickets");
+        }
+        else{
+            System.out.println("Required more money");
+
+        }
+    }   
+}
+
+/*Output:
+ * compile time success 
+ * run time success
+ * You can buy tickets
+ */
+
+
