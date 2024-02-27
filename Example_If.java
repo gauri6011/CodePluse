2bd8269ded86959ab0367815887e4934e5b3aa87
Gauri Bhagwat <157735967+gauri6011@users.noreply.github.com>
2024-02-27 13:23:50 +0530

    Add files via upload

---
+++ Example_If.java
@@ -0,0 +1,17 @@
+ class Example_If {
+    public static void main(String[] args) {
+        int age=20;
+        // if conditional statement
+        if(age>=18)
+        {
+            System.out.println("You are Eligible for voting");
+        }
+    }
+    
+}
+
+/*Output:
+ * compile time success
+ * runtime success
+ * You are Eligible for voting
+ */
