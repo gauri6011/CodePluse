d147eed7f3135abc1b68de6499b588baca7b5b8a
Gauri Bhagwat <157735967+gauri6011@users.noreply.github.com>
2024-03-01 22:14:48 +0530

    Add files via upload

---
+++ BasicEgSwitch.java
@@ -0,0 +1,34 @@
+ class BasicEgSwitch
+{
+    public static void main(String[] args)
+    {
+        int dayNum=2;
+        // this is fall through case
+         switch(dayNum)
+         {
+
+            case 1:System.out.println("Monday");
+            case 2:System.out.println("Tuesday");
+            case 3:System.out.println("Wednesday");
+            case 4:System.out.println("Thursday");
+            case 5:System.out.println("Friday");
+            case 6:System.out.println("Saturday");
+            case 7:System.out.println("Sunday");
+
+            default : System.out.println("Invalid");
+
+         }    
+    }
+}
+
+/* OUTPUT:
+compile time success
+run time success
+Tuesday
+Wednesday
+Thursday
+Friday
+Saturday
+Sunday
+Invalid
+ */
