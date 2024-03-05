23531c0e644060c27173c5ad3ff44f91a62b4103
Gauri Bhagwat <157735967+gauri6011@users.noreply.github.com>
2024-03-05 21:22:25 +0530

    Add files via upload

---
+++ DoWhileExample.java
@@ -0,0 +1,32 @@
+class DoWhileExample 
+{
+    public static void main(String[] args)
+    {
+       // declaratiin
+        int a=0;
+        do
+        {
+            //task
+            System.out.println(a);
+            //updation
+            a++;
+        } while(a<=10);     //condition
+    }
+    
+}
+
+/*
+output:
+comple time success
+run time success
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+ */
