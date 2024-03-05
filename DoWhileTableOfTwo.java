163c3510c6d5153bc619a0629181f2c04b1e1926
Gauri Bhagwat <157735967+gauri6011@users.noreply.github.com>
2024-03-05 21:22:56 +0530

    Add files via upload

---
+++ DoWhileTableOfTwo.java
@@ -0,0 +1,27 @@
+class DoWhileTableOfTwo 
+{
+    public static void main(String[] args) {
+        int a=1;
+        do{
+            System.out.println(a*2);
+            a++;
+        }while(a<=10);
+    }
+    
+}
+
+/*
+ * output:
+comple time success
+run time success
+2
+4
+6
+8
+10
+12
+14
+16
+18
+20
+ */
