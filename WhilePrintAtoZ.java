18cae669369020714271a8aa4fbe2f3d58b298f8
Gauri Bhagwat <157735967+gauri6011@users.noreply.github.com>
2024-03-08 22:19:41 +0530

    Add files via upload

---
+++ WhilePrintAtoZ.java
@@ -0,0 +1,43 @@
+public class WhilePrintAtoZ {
+    public static void main(String[] args) {
+        char a='A';
+        while(a<='Z')
+        {
+            System.out.println(a);
+            a++;
+        }
+    }
+}
+
+/*
+ * output:
+  compile time success
+  run time success
+  A
+B
+C
+D
+E
+F
+G
+H
+I
+J
+K
+L
+M
+N
+O
+P
+Q
+R
+S
+T
+U
+V
+W
+X
+Y
+Z
+ */
+
