58cbcdf2b03bd14c0049f07672962c25c880d0d7
Gauri Bhagwat <157735967+gauri6011@users.noreply.github.com>
2024-03-09 23:16:03 +0530

    Add files via upload

---
+++ SumOfNnaturalNumbers.java
@@ -0,0 +1,19 @@
+import java. util.*;
+class SumOfNnaturalNumbers {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the value of N:");
+        int N=sc.nextInt();
+        int Sum=N*(N+1)/2;
+        System.out.println("Sum is:" +Sum);
+
+    }
+    
+}
+/*output: 
+comple time success
+run time success
+Enter the value of N:
+45
+Sum is:1035
+*/
