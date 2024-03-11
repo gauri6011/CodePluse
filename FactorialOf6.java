df2f734aca930066f57ad723d6e0383e9de64334
Gauri Bhagwat <157735967+gauri6011@users.noreply.github.com>
2024-03-11 20:03:45 +0530

    Add files via upload

---
+++ FactorialOf6.java
@@ -0,0 +1,19 @@
+public class FactorialOf6 {
+    public static void main(String[] args) {
+        int i=6;
+        int fact=1;
+        while(i>0){
+             fact=fact*i;
+             i--;
+        }
+        System.out.println(fact);
+        }
+    }
+    /*Output:
+    compile time success
+    runtime success
+    720
+
+     */
+    
+
