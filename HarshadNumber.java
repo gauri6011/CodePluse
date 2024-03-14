52372eef7152eab06c840862dc0e6f325e85a6e9
Gauri Bhagwat <157735967+gauri6011@users.noreply.github.com>
2024-03-14 18:21:45 +0530

    Add files via upload

---
+++ HarshadNumber.java
@@ -0,0 +1,24 @@
+public class HarshadNumber {
+    public static void main(String[] args) {
+        int num=21;
+        int num1=num;
+        int sum=0;
+        while(num>0){
+            int rem=num%10;
+            sum=sum+rem;
+            num=num/10;
+        }
+        if(num1%sum==0){
+            System.out.println("Given number is harshad Number");
+        }
+        else{
+            System.out.println("Given number is not harshad Number");
+        }
+    }
+}
+
+/*output:
+compile time success
+run time success
+The LCM of given number is 36
+*/
