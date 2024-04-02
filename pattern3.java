792f21a31bb8aa34f3474a5c0279fa5755657358
Gauri Bhagwat <157735967+gauri6011@users.noreply.github.com>
2024-04-02 21:59:34 +0530

    Add files via upload

---
+++ pattern3.java
@@ -0,0 +1,21 @@
+public class pattern3 {
+    public static void main(String[] args) {
+        for(int i=1; i<=4;i++) //row
+        {
+            for(int j=1; j<=5;j++)//col
+            {
+                 System.out.print("*");
+            }
+            System.out.println( );
+        }
+    }
+}
+
+/*Output:
+compile time success
+run time success
+*****
+*****
+*****
+*****
+ */
