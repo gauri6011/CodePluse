68568e801eff16f1cb4cdac42900d291342b889f
Gauri Bhagwat <157735967+gauri6011@users.noreply.github.com>
2024-04-02 21:53:53 +0530

    Add files via upload

---
+++ Pattern_Diagonal.java
@@ -0,0 +1,28 @@
+public class Pattern_Diagonal {
+    public static void main(String[] args) {
+        
+     int a=6;
+    for (int i=1; i<=6; i++){
+        for(int j=1; j<=6; j++){
+            if (j==(a-i+1) || i == 1 || j==1 || i==6 || j==6 ){
+                System.out.print(" * ");
+            } else{
+                System.out.print("   ");
+            }
+        }
+        System.out.println();
+     }
+}
+}
+  
+
+/*OutPut:
+compile time success
+run time success
+ *  *  *  *  *  * 
+ *           *  *
+ *        *     *
+ *     *        *
+ *  *           *
+ *  *  *  *  *  *
+ */
