6951861953b9eee48f2eba2242ae0aa3f20d6356
Gauri Bhagwat <157735967+gauri6011@users.noreply.github.com>
2024-04-02 21:55:00 +0530

    Add files via upload

---
+++ Pattern2.java
@@ -0,0 +1,23 @@
+public class Pattern2 {
+    public static void main(String[] args) {
+        for(int i=1; i<=4;i++) //row
+        {
+            for(int j=1; j<=2;j++)//col
+            {
+                 System.out.print("*");
+            }
+            System.out.println( );
+        }
+    }
+}
+
+/* 
+ compile time success
+ run time success
+
+**
+**
+**
+**
+
+*/
