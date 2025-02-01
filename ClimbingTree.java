c80eb958b74fd88b845c7f42f08d54074cd66393
Gauri Bhagwat <157735967+gauri6011@users.noreply.github.com>
2025-02-01 22:43:02 +0530

    Create ClimbingTree.java

---
+++ ClimbingTree.java
@@ -0,0 +1,20 @@
+class ClimbingTree {
+    public int climbStairs(int n) {
+        if (n == 1) return 1;
+        if (n == 2) return 2;
+
+        int prev1 = 2, prev2 = 1;
+        for (int i = 3; i <= n; i++) {
+            int current = prev1 + prev2;
+            prev2 = prev1;
+            prev1 = current;
+        }
+        return prev1;
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int n = 5; 
+        System.out.println("Number of ways to climb " + n + " stairs: " + solution.climbStairs(n));
+    }
+}
